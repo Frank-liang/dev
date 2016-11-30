6dfc48d129b6c85e704a3ab2462eee289722dcbe
root <root@zcool_manager.(none)>
2016-11-30 16:57:44 +0800

    add db global config

--- runapi.py
+++ runapi.py
@@ -3,11 +3,14 @@
 
 from api import app
 import utils
+import db
 #导入自定义的各种配置参数，最终参数以字典形式返回
 config = utils.get_config('api')
 
 #将自定义的配置文件全部加载到全局的配置文件(app.config),可以在任意地方调用
 app.config.update(config)
+app.config['db'] = db.Cursor(config)
+print app.config
 
 #print app.config
 utils.write_log('web').info("just a test")
